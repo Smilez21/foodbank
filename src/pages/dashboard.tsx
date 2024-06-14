f65c18689a5ed94adb72f05e3a16f7189177f27e
Smilez <virtuousajor394@gmail.com>
2024-06-14 12:48:15 +0100

    first commit

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -29,7 +29,7 @@ const Dashboard: React.FC = () => {
         <button
           onClick={handleLogout}  
           className="bg-red-500 text-white px-4 py-2 rounded mt-4" 
-        >
+        > 
           Logout
         </button>
       </div>
